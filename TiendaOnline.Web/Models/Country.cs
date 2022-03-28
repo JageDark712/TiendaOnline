7034e6ccd68adf237d53579e013eaf230166aa49
Jager <darkliner2013@gmail.com>
2022-03-28 18:17:38 -0500

    update v2

--- TiendaOnline.Web/Models/Country.cs
+++ TiendaOnline.Web/Models/Country.cs
@@ -6,7 +6,7 @@ namespace TiendaOnline.Web.Models
     {
         public int Id { get; set; }
         [Required]
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "El campo{0} debe contener al  menos un caracter")]
         public string Name { get; set; }
     }
 }
