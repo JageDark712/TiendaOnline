da10d0a745b520dbf177b89fb02e8f2852346ceb
Jager <darkliner2013@gmail.com>
2022-03-28 17:47:18 -0500

    Add project v1

---
+++ TiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Web.Models;
+
+namespace TiendaOnline.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
